da241060141785b447de710aa61ae8f27285f5c2
Vadim <www.vadim.2@mail.ru>
2022-05-25 16:17:19 +0300

    0.2

--- calculator.js
+++ calculator.js
@@ -1,125 +1,54 @@
-let input = document.getElementById('input');
-//addListener, тру убрать, классы убрать(ид), ретурны, ифы убрать?, создать массив операторов и переделать условия
-const plus = document.getElementById('plus');
-const reset = document.getElementById('reset');
-const subtract = document.getElementById('subtract');
-const negative = document.getElementById('negative');
-const percent = document.getElementById('percent');
-const division = document.getElementById('division');
-const seven = document.getElementById('seven');
-const eight = document.getElementById('eight');
-const nine = document.getElementById('nine');
-const multiplication = document.getElementById('multiplication');
-const four = document.getElementById('four');
-const five = document.getElementById('five');
-const six = document.getElementById('six');
-const one = document.getElementById('one');
-const two = document.getElementById('two');
-const three = document.getElementById('three');
-const zero = document.getElementById('zero');
-const comma = document.getElementById('comma');
-const equals = document.getElementById('equals');
+const panel = document.getElementById('panel');
+const input = document.getElementById('input');
+const arrayOperation = ['+', '-', '/', '*'];
 
-plus.addEventListener('click',addFunction);
-reset.addEventListener('click',resetFunction);
-subtract.addEventListener('click',subtractFunction);
-negative.addEventListener('click',negativeFunction);
-percent.addEventListener('click',addPercentFunction);
-division.addEventListener('click',divisionFunction);
-multiplication.addEventListener('click',multiplicationFunction);
-comma.addEventListener('click',commaFunction);
-equals.addEventListener('click',equalsFunction);
-one.addEventListener('click',addOneFunction);
-two.addEventListener('click',addOneFunction);
-three.addEventListener('click',addOneFunction);
-four.addEventListener('click',addOneFunction);
-five.addEventListener('click',addOneFunction);
-six.addEventListener('click',addOneFunction);
-seven.addEventListener('click',addOneFunction);
-eight.addEventListener('click',addOneFunction);
-nine.addEventListener('click',addOneFunction);
-zero.addEventListener('click',addOneFunction);
+panel.addEventListener('click',(event) => {
+    const btn = event.target.id;
+    const lastSymbol = input.value[input.value.length - 1];
 
-
-function addFunction () {
-    let b = input.value.split('');
-    if (!(b.includes('+', length - 1)) && !(b.includes('-', length - 1)) &&
-        !(b.includes('/', length - 1)) && !(b.includes('*', length - 1))) {
-        input.value = input.value + '+';
-    } else {
-        input.value = input.value.slice(0, -1);
-        input.value = input.value + '+';
+    if (arrayOperation.includes(lastSymbol) && arrayOperation.includes(btn)) {
+        input.value = input.value.slice(0,-1) + btn;
+        return;
     }
-}
-function subtractFunction () {
-        let b = input.value.split('');
-        if (!(b.includes('+', length - 1)) && !(b.includes('-', length - 1)) &&
-            !(b.includes('/', length - 1)) && !(b.includes('*', length - 1))) {
-            input.value = input.value + '-';
-        } else {
-            input.value = input.value.slice(0, -1);
-            input.value = input.value + '-';
-        }
-    }
-function multiplicationFunction () {
-    let b = input.value.split('');
-          if (!(b.includes('+', length - 1)) && !(b.includes('-', length - 1)) &&
-              !(b.includes('/', length - 1)) && !(b.includes('*', length - 1))) {
-             input.value = input.value + '*';
-         } else {
-             input.value = input.value.slice(0, -1);
-             input.value = input.value + '*';
-         }
-}
-function divisionFunction () {
-    let b = input.value.split('');
-          if (!(b.includes('+', length - 1)) && !(b.includes('-', length - 1)) &&
-              !(b.includes('/', length - 1)) && !(b.includes('*', length - 1))) {
-             input.value = input.value + '/';
-         } else {
-             input.value = input.value.slice(0, -1);
-             input.value = input.value + '/';
-         }
-}
-function addPercentFunction () {
-         let b = input.value.split('');
-         if (!(b.includes('+', length - 1)) && !(b.includes('-', length - 1)) &&
-             !(b.includes('/', length - 1)) && !(b.includes('*', length - 1))) {
-            input.value = +input.value / 100;
-       } else {
-       }
-}
-function negativeFunction () {
-    let b = input.value.split('');
-    if (!(b.includes('+', length - 1)) && !(b.includes('-', length - 1)) &&
-        !(b.includes('/', length - 1)) && !(b.includes('*', length - 1))) {
-        input.value = +input.value * -1;
-    } else {
-    }
-}
-function resetFunction() {
-    input.value = '';
-}
-function equalsFunction () {
-    let b = input.value.split('');
-    if (!(b.includes('+', length - 1)) && !(b.includes('-', length - 1)) &&
-        !(b.includes('/', length - 1)) && !(b.includes('*', length - 1))) {
+    if (btn === '=') {
         input.value = eval(input.value);
-        function sayHi() {
-            input.value = '';
-        }
-        setTimeout(sayHi, 10000);
-    } else {
+        return;
     }
-}
-function commaFunction() {
-         let b = input.value.split('');
-         if (!(b.includes('+', length - 1)) && !(b.includes('-', length - 1)) &&
-             !(b.includes('/', length - 1)) && !(b.includes('*', length - 1))) {
-             input.value = +input.value + '.';
-         } else {
+    if (btn === 'reset') {
+        input.value = '';
+        return;
+    }
+    if (btn === '.') {
+        if (lastSymbol === '.') {
+            return;
         }
-}
-function addOneFunction() {
-    input.value = input.value + event.target.innerHTML;
-}
+        if (input.value.indexOf("+") !== -1) {
+            if (input.value.slice(input.value.indexOf("+")).includes(".") !== false) {
+                return
+            }
+        } else if (input.value.includes('.') === true) {
+            return;
+        }
+        if (input.value.indexOf("-") !== -1) {
+            if (input.value.slice(input.value.indexOf("+")).includes(".") !== false) {
+                return
+            }
+        }
+        if (input.value.indexOf("/") !== -1) {
+            if (input.value.slice(input.value.indexOf("+")).includes(".") !== false) {
+                return
+            }
+        }
+        if (input.value.indexOf("*") !== -1) {
+            if (input.value.slice(input.value.indexOf("+")).includes(".") !== false) {
+                return
+            }
+        }
+    }
+    if (btn === '%') {
+        input.value = '';
+        return;
+    }
+    input.value = input.value + btn;
+})
+
