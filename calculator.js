c4a93f9616733f8d09fd47eddd26cc49ebd23236
Vadim <www.vadim.2@mail.ru>
2022-05-31 20:29:31 +0300

    0.2

--- calculator.js
+++ calculator.js
@@ -5,7 +5,12 @@ const arrayOperation = ['+', '-', '/', '*'];
 panel.addEventListener('click',(event) => {
     const btn = event.target.id;
     const lastSymbol = input.value[input.value.length - 1];
+    console.log(lastSymbol);
 
+    if ((arrayOperation.includes(btn) === true) && (input.value === '')){
+        input.value = input.value.substring(0, input.value.length - 1);
+        return;
+    }
     if (arrayOperation.includes(lastSymbol) && arrayOperation.includes(btn)) {
         input.value = input.value.slice(0,-1) + btn;
         return;
@@ -43,6 +48,14 @@ panel.addEventListener('click',(event) => {
         input.value = input.value/100;
         return;
     }
+    if (btn === 'negative') {
+        if (arrayOperation.includes(lastSymbol)) {
+            return;
+        }
+        input.value = eval(input.value);
+        input.value = input.value * (-1);
+        return;
+    }
     input.value = input.value + btn;
 })
 
@@ -65,3 +78,51 @@ function checkOperation () {
         return input.value.indexOf(e) === -1;
     });
 }
+
+document.addEventListener('keypress',(e) => {
+    input.value = input.value.replace (/[^0-9-+-=/*%]/g, '');
+     const lastSymbol = input.value[input.value.length - 1];
+     const qwe = ("+" === e.key ||
+         "/" === e.key ||
+         "*" === e.key ||
+         "." === e.key ||
+         "%" === e.key ||
+         "=" === e.key ||
+         "-" === e.key);
+
+     if (input.value === '' && qwe) {
+         input.value = input.value.substring(0, input.value.length - 1);
+         return;
+     }
+     if (qwe && (lastSymbol === e.key)){
+         input.value = input.value.substring(0, input.value.length - 1) + e.key;
+         return;
+     }
+    switch (e.key){
+        case "Enter":
+            input.value = eval(input.value);
+            break;
+        case "%":
+            if (arrayOperation.includes(lastSymbol)) {
+                return;
+            }
+            input.value = input.value/100;
+            break;
+        default:
+            input.value = input.value + e.key;
+            break;
+    }
+})
+document.addEventListener('keydown',(e) => {
+    switch (e.key) {
+        case "Delete":
+            input.value = '';
+            break;
+        case "Escape":
+            input.value = '';
+            break;
+        case "Backspace":
+            input.value = input.value.substring(0, input.value.length - 1);
+            break;
+    }
+})
